aac97ea2efa096585d281b252817d5eb11b46dc7
rajeshsundar31 <rajeshsundarjr10@gmail.com>
2024-11-24 13:42:45 +0530

    Login page completed

--- lib/core/utils/asset_utils.dart
+++ lib/core/utils/asset_utils.dart
@@ -4,4 +4,7 @@ class AssetsUtils {
   static String landingAvatar = "$rootPath/avatar.jpg";
   static String myProfile = "$rootPath/dSC1410.jpg";
   static String payment = "$rootPath/PaymentInfo.png";
+  static String payBro = "$rootPath/Credit card-bro.png";
+  static String payColor = "$rootPath/color.png";
+
 }
--- lib/presentation/login/login_view.dart
+++ lib/presentation/login/login_view.dart
@@ -2,7 +2,9 @@ import 'package:finsight/component/custom_button.dart';
 import 'package:finsight/core/constants/app_colors.dart';
 import 'package:finsight/core/constants/app_strings.dart';
 import 'package:finsight/core/utils/app_size.dart';
+import 'package:finsight/core/utils/asset_utils.dart';
 import 'package:finsight/presentation/login/login_view_model.dart';
+import 'package:finsight/presentation/register/register_view.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,66 +17,68 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-
   final _formKey = GlobalKey<FormState>();
-   String? email;
+  String? email;
   String? password;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: _buildBody(context),
-      )
-      );
+        child: Scaffold(
+      body: _buildBody(context),
+    ));
   }
 
-  Widget _buildEmail(BuildContext context){
-    return SizedBox(
-      width: displayWidth(context) / 1.2,
-      child: TextFormField(
-        decoration: const InputDecoration(
-          labelText: "Email",
-          hintText: "Enter Your Email",
-          border: OutlineInputBorder()
+  Widget _buildEmail(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: displayWidth(context) / 1.2,
+        child: TextFormField(
+          decoration: const InputDecoration(
+              labelText: "Email",
+              hintText: "Enter Your Email",
+              border: OutlineInputBorder()),
+          keyboardType: TextInputType.emailAddress,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter an email';
+            } else if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+              return 'Please enter a valid email';
+            }
+            return null; // Return null if no error
+          },
+          onChanged: (value) {
+            email = value;
+          },
         ),
-        keyboardType: TextInputType.emailAddress,
-        validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an email';
-                  } else if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                    return 'Please enter a valid email';
-                  }
-                  return null; // Return null if no error
-                },
-                onChanged: (value) {
-                     email = value;
-                },
       ),
     );
   }
 
-   Widget _buildPassword(BuildContext context){
-    return SizedBox(
-      width: displayWidth(context) / 1.2,
-      child: TextFormField(
-        decoration: const InputDecoration(
-          labelText: "Password",
-          hintText: "Enter Your Password",
-          border: OutlineInputBorder()
+  Widget _buildPassword(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: displayWidth(context) / 1.2,
+        child: TextFormField(
+          decoration: const InputDecoration(
+              labelText: "Password",
+              hintText: "Enter Your Password",
+              border: OutlineInputBorder()),
+          obscureText: true,
+          keyboardType: TextInputType.visiblePassword,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter valid password';
+            } else if (value.length < 4) {
+              return 'Please enter a valid password';
+            }
+            return null; // Return null if no error
+          },
+          onChanged: (value) {
+            password = value;
+          },
         ),
-        obscureText: true,
-        keyboardType: TextInputType.visiblePassword,
-        validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter valid password';
-                  } else if (value.length< 4) {
-                    return 'Please enter a valid password';
-                  }
-                  return null; // Return null if no error
-                },
-        onChanged: (value) {
-          password = value;
-        },
       ),
     );
   }
@@ -84,13 +88,12 @@ class _LoginViewState extends State<LoginView> {
       return Column(
         children: [
           CustomButton(
-            color: AppColor.purple,
+            color: AppColor.btn_green,
             text: AppStrings.continueBtn,
             onPressed: () {
-              print("email${email}");
-              print("pass${password}");
               if (_formKey.currentState!.validate()) {
-                btnLogin.validateLogin(email: email!, password: password!, context: context);
+                btnLogin.validateLogin(
+                    email: email!, password: password!, context: context);
               }
             },
             width: displayWidth(context) / 1.3,
@@ -100,45 +103,65 @@ class _LoginViewState extends State<LoginView> {
     });
   }
 
-  Widget _buildBottomContent(BuildContext context){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        RichText(text: TextSpan(
-          children: [
-            const TextSpan(text: "Don't Have a Account ?",style: TextStyle(color: AppColor.black)),
-            const WidgetSpan(child: SizedBox(width: 8.0,)),
-            TextSpan(text: "Register",style: const TextStyle(color: AppColor.green), 
-            recognizer: TapGestureRecognizer()
-            ..onTap = () {
-            }
-            )
-          ]
-        )),
-      ],
+  Widget _buildBottomContent(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RichText(
+              text: TextSpan(children: [
+            const TextSpan(
+                text: "Don't Have a Account ?",
+                style: TextStyle(color: AppColor.black)),
+            const WidgetSpan(
+                child: SizedBox(
+              width: 8.0,
+            )),
+            TextSpan(
+                text: "Register",
+                style: const TextStyle(color: AppColor.green),
+                recognizer: TapGestureRecognizer()..onTap = () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterUser()));
+                })
+          ])),
+        ],
+      ),
     );
   }
 
-  Widget _buildContainer(context){
+  Widget _buildContainer(context) {
     return Container(
       width: displayWidth(context),
-      height: displayHeight(context) * 0.32,
-      color: AppColor.green,
+      height: displayHeight(context) * 0.42,
+      decoration: const BoxDecoration(
+        // color: AppColor.btn_green,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+      ),
+      child: Image.asset(AssetsUtils.payColor)
     );
   }
 
-  Widget _buildBody(BuildContext context){
+  Widget _buildContent(context){
     return Form(
       key: _formKey,
       child: Column(
         children: [
-          _buildContainer(context),
           _buildEmail(context),
           _buildPassword(context),
           _buildButton(context),
           _buildBottomContent(context),
         ],
-      ),
+      )
+      );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Column(
+      children: [
+        _buildContainer(context),
+        _buildContent(context)
+      ],
     );
   }
 }
