5a4defba8d9e7cfad5443f63f273b1661a01edbc
rajeshsundar31 <rajeshsundarjr10@gmail.com>
2025-08-02 16:44:39 +0530

    changes

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,16 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:mobile_scanner/src/web/mobile_scanner_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ImagePickerPlugin.registerWith(registrar);
+  MobileScannerWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
